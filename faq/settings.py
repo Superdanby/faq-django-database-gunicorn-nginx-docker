46480512907b0e0b06878b15971afc6349aa803e
Superdanby <dannymrt1@gmail.com>
2020-01-08 17:18:23 +0800

    Serve static files correctly

--- faq/settings.py
+++ faq/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
 
 # MEDIA
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
