5787a8234c5dad0637a6098052b35d3e05cceed5
Superdanby <dannymrt1@gmail.com>
2020-01-16 11:35:27 +0800

    FAQs can be edit/delete by authors & superusers

--- faqsystem/admin.py
+++ faqsystem/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 
 # Register your models here.
 from .models import FAQ, Images, Files
@@ -22,14 +23,34 @@ class FAQAdmin(admin.ModelAdmin):
         for obj in queryset:
             if obj.author == request.user:
                 obj.delete()
+            elif request.user.is_superuser:
+                obj.delete()
+                messages.success(request, "You are SUPERUSER nothing can stop you.")
+            else:
+                messages.set_level(request, messages.ERROR)
+                messages.error(request, obj.question_text + " created by " + obj.author.get_username() + ". Only the author can edit or delete it.")
 
     def save_model(self, request, obj, form, change):
-        obj.author = request.user
-        super().save_model(request, obj, form, change)
+        if obj.author is None or obj.author == request.user:
+            obj.author = request.user
+            super().save_model(request, obj, form, change)
+        elif request.user.is_superuser:
+            obj.author = request.user
+            super().save_model(request, obj, form, change)
+            messages.success(request, "You are SUPERUSER nothing can stop you.")
+        else:
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, obj.question_text + " created by " + obj.author.get_username() + ". Only the author can edit or delete it.")
 
     def delete_model(self, request, obj):
         if obj.author == request.user:
             super().delete_model(request, obj)
+        elif request.user.is_superuser:
+            super().delete_model(request, obj)
+            messages.success(request, "You are SUPERUSER nothing can stop you.")
+        else:
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, obj.question_text + " created by " + obj.author.get_username() + ". Only the author can edit or delete it.")
 
 
 admin.site.register(FAQ, FAQAdmin)
